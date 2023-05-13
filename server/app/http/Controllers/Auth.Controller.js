30a09f861f57c362757bbcdb48b73129b952c905
Mani Ramezanpour <maniramznpoor2007@gmail.com>
2023-05-13 17:14:41 +0330

    done login system

--- server/app/http/Controllers/Auth.Controller.js
+++ server/app/http/Controllers/Auth.Controller.js
@@ -1,32 +1,55 @@
+const { token } = require("morgan");
 const { UserModel } = require("../../Model/User.Model");
 const { hashString } = require("../../Modules/hashString");
+const bcrypt = require("bcrypt");
+const { tokenGenerator } = require("../../Modules/jwt");
 
 class AuthController {
   async Register(req, res, next) {
     try {
-      
-   
-    const { username, password, email, mobile } = req.body;
-    const hashedPassword = hashString(password);
-    const user = await UserModel.create({
-      email,
-      username,
-      password: hashedPassword,
-      mobile,
-    }).catch((err) => {
-      if (err?.code == 11000) {
-        throw {
-          status: 400,
-          message: "username is used please select another",
-        };
-      }
-    });
-    return res.json(user);
-  } catch (error) {
-   next(error)   
+      const { username, password, email, mobile } = req.body;
+      const hashedPassword = hashString(password);
+      const user = await UserModel.create({
+        email,
+        username,
+        password: hashedPassword,
+        mobile,
+      }).catch((err) => {
+        if (err?.code == 11000) {
+          throw {
+            status: 400,
+            message: "username is used please select another",
+          };
+        }
+      });
+      return res.json(user);
+    } catch (error) {
+      next(error);
+    }
   }
+  async Login(req, res, next) {
+    try {
+      const { username, password } = req.body;
+      const user = await UserModel.findOne({ username });
+      if (!user)
+        throw { status: 404, message: "username or password is false" };
+
+      const comparerResult = bcrypt.compareSync(password, user.password);
+      const token = tokenGenerator({ username });
+      user.token = token;
+      user.save();
+      if (!comparerResult)
+        throw { status: 404, message: "username or password is false" };
+      return res.status(200).json({
+        status: 202,
+        success: true,
+        message: "your login is success",
+        token: tokenGenerator({username}),
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-  Login() {}
 
   ResetPassword() {}
 }
