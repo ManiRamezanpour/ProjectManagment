5fa55756277df8ef2cce59203a61065c04ea0bb1
Mani Ramezanpour <maniramznpoor2007@gmail.com>
2023-05-23 10:03:13 +0330

    update user function

--- server/app/Router/User.Route.js
+++ server/app/Router/User.Route.js
@@ -3,6 +3,8 @@ const { UserController } = require("../http/Controllers/User.Controller");
 const router = require("express").Router();
 
 router.get("/profile", checkLogin, UserController.getProfile);
+router.post("/profile", checkLogin, UserController.editProfile);
+
 module.exports = {
   userRoutes: router,
 };
--- server/app/http/Controllers/User.Controller.js
+++ server/app/http/Controllers/User.Controller.js
@@ -1,3 +1,4 @@
+const { UserModel } = require("../../Model/User.Model");
 class UserController {
   async getProfile(req, res, next) {
     try {
@@ -12,7 +13,31 @@ class UserController {
       next(error);
     }
   }
-  editProfile() {}
+  async editProfile(req, res, next) {
+    try {
+      const data = { ...req.body };
+      console.log(data);
+      const userId = req.user._id;
+      Object.entries(data).forEach(([key, value]) => {
+        const feilds = ["firstname", "username", "skills"];
+        const badValues = ["", " ", null, undefined, [], {}, 0, -1, NaN];
+        if (!feilds.includes(key)) delete data[key];
+        if (badValues.includes(value)) delete data[key];
+      });
+
+      const result = await UserModel.updateOne({ _id: userId }, { $set: data });
+      if (result.modifiedCount > 0) {
+        return res.status(200).json({
+          status: 200,
+          success: true,
+          message: "account updated !!",
+        });
+      }
+      throw { status: 400, message: "updated not finished !" };
+    } catch (error) {
+      next();
+    }
+  }
   addSkills() {}
   editSkills() {}
   acceptInviteToTeam() {}
